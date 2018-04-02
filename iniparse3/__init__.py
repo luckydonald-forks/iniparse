38eb3b5f65451774c5671ad1d2a03d189a816207
luckydonald <luckydonald@flutterb.at>
2018-04-02 14:49:53 +0200

    This should fix python 3

--- iniparse3/__init__.py
+++ iniparse3/__init__.py
@@ -1,6 +1,7 @@
 # Copyright (c) 2001, 2002, 2003 Python Software Foundation
 # Copyright (c) 2004-2008 Paramjit Oberoi <param.cs.wisc.edu>
 # Copyright (c) 2007 Tim Lauridsen <tla@rasmil.dk>
+# Copyright (c) 2018 luckydonald <https://github.com/luckydonald>
 # All Rights Reserved.  See LICENSE-PSF & LICENSE for details.
 
 from ini import INIConfig, change_comment_syntax
@@ -8,12 +9,15 @@ from config import BasicConfig, ConfigNamespace
 from compat import RawConfigParser, ConfigParser, SafeConfigParser
 from utils import tidy
 
-from ConfigParser import DuplicateSectionError,    \
-                  NoSectionError, NoOptionError,   \
-                  InterpolationMissingOptionError, \
-                  InterpolationDepthError,         \
-                  InterpolationSyntaxError,        \
-                  DEFAULTSECT, MAX_INTERPOLATION_DEPTH
+try:
+    # python 3
+    from configparser import DuplicateSectionError, NoSectionError, NoOptionError, InterpolationMissingOptionError, InterpolationDepthError, InterpolationSyntaxError, 
+DEFAULTSECT, MAX_INTERPOLATION_DEPTH
+except ImportError:
+    # python 2
+    from ConfigParser import DuplicateSectionError, NoSectionError, NoOptionError, InterpolationMissingOptionError, InterpolationDepthError, InterpolationSyntaxError, 
+DEFAULTSECT, MAX_INTERPOLATION_DEPTH
+# end try
 
 __all__ = [
     'BasicConfig', 'ConfigNamespace',
@@ -23,4 +27,5 @@ __all__ = [
     'InterpolationMissingOptionError', 'InterpolationDepthError',
     'InterpolationSyntaxError', 'DEFAULTSECT', 'MAX_INTERPOLATION_DEPTH',
 ]
+
 
